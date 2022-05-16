fe3212a58a8ef4e62ab692f1fed7e97467669b05
jh-kict96 <jinho9610@gmail.com>
2022-05-16 17:03:43 +0900

    edit python sample - error handling logic of slack alert

--- samples/python/slack/__main__.py
+++ samples/python/slack/__main__.py
@@ -8,15 +8,18 @@ def main(args):
     author = commit_info["author"]["name"]
     commit_msg = commit_info["message"]
 
-    alert_data = {"text": f"author({author}) commited on repo({repo_name}) with msg({commit_msg})"}
+    alert_data = {
+        "text": f"author({author}) commited on repo({repo_name}) with msg({commit_msg})"
+    }
 
     try:
         res = requests.post(args["slack_url"], data=json.dumps(alert_data))
 
         if res.status_code == 200:
-            return {"done": True, "status": res.status_code}
+            return {"done": True}
+
         else:
-            return {"done": False, "status": res.status_code}
+            raise Exception({"done": False, "error_message": res.text})
 
     except Exception as e:
-        return {"done": False, "error_message": e}
+        raise Exception({"done": False, "error_message": str(e)})
